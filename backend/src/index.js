5a318c1bb1ea42b63172624d3cba8944ca4fcd78
SkamAbhi <abhisingh3476@gmail.com>
2024-11-30 18:06:51 +0530

    feat:design-updates

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieparser from "cookie-parser";
-
+import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
@@ -12,7 +12,12 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cookieparser());
-
+app.use(
+  cors({
+    origin: "http://localhost:5001",
+    credentials: true,
+  })
+);
 const PORT = process.env.PORT || 5001;
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import {BrowerRouter} from "react-router-dom"
+import {    BrowserRouter } from "react-router-dom"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowerRouter>
-    <App />
-    </BrowerRouter>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
   </StrictMode>,
 )
